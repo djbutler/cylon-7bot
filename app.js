4557f5ecf191320856af2ee41101bb5b4c95aa5e
Dan Butler <djbutler@cs.washington.edu>
2016-05-01 11:07:36 -0700

    added stubs for reading servo positions

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ Cylon.robot({
     joint3: { driver: 'servo', pin: 5 },
     joint4: { driver: 'servo', pin: 6 },
     joint5: { driver: 'servo', pin: 7 },
+    joint6: { driver: 'servo', pin: 8 },
+    jointsensor0: { driver: 'analogSensor', pin: 0, lowerLimit: 100, upperLimit: 900 },
+    jointsensor1: { driver: 'analogSensor', pin: 1, lowerLimit: 100, upperLimit: 900 },
+    jointsensor2: { driver: 'analogSensor', pin: 2, lowerLimit: 100, upperLimit: 900 },
+    jointsensor3: { driver: 'analogSensor', pin: 3, lowerLimit: 100, upperLimit: 900 },
+    jointsensor4: { driver: 'analogSensor', pin: 4, lowerLimit: 100, upperLimit: 900 },
+    jointsensor5: { driver: 'analogSensor', pin: 5, lowerLimit: 100, upperLimit: 900 },
   },
 
   work: function() {
